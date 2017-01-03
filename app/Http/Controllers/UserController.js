ddba5111579f61188a4925765255dc28940860e4
Lipcsei Ádám <Lipcsei dm>
2017-01-03 20:11:43 +0100

    3.beadando

--- app/Http/Controllers/UserController.js
+++ app/Http/Controllers/UserController.js
@@ -152,6 +152,43 @@ class UserController {
     }
   }
 
+
+  * ajaxRegister(request, response){
+        const registerData = request.except('_csrf');
+        const rules = {
+            name: 'required',
+            username: 'required|alpha_numeric|unique:users',
+            email: 'required|email|unique:users',
+            roomnumber: 'required|alpha_numeric',
+            password: 'required|min:4',
+            password_confirm: 'required|same:password',
+        }
+        const validation = yield Validator.validateAll(registerData, rules);
+        if (validation.fails()) {
+            yield request
+            .withAll()
+            .andWith({ errors: validation.messages() })
+            .flash()
+
+            response.redirect('back')
+            return
+        }
+
+        const user = new User();
+
+        user.name = registerData.name
+        user.username = registerData.username
+        user.email = registerData.email
+        user.roomnumber = registerData.roomnumber
+        user.password = yield Hash.make(registerData.password)
+
+        yield user.save()
+
+        yield request.auth.login(user)
+
+        response.redirect('/');
+  }
+
 }
 
 module.exports = UserController
